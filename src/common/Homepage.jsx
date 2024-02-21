e404c83b6e9f244369702e71a39306383f64b2de
Krishna <89905895+krishna7288@users.noreply.github.com>
2024-02-21 19:18:54 +0530

    Breadcrumbs added

--- src/common/Homepage.jsx
+++ src/common/Homepage.jsx
@@ -1,6 +1,8 @@
 import Adminsidebar from "./Sidebar";
 import { Outlet, useNavigate } from "react-router-dom";
 import { IoMdLogOut } from "react-icons/io";
+import { useLocation } from 'react-router-dom';
+import Breadcrumbs from "../components/utils/Breadcrumbs";
 
 const Homepage = () => {
     let navigate = useNavigate()
@@ -9,6 +11,12 @@ const Homepage = () => {
     localStorage.removeItem("chitsRole");
     // navigate('/')
   };
+
+  const location = useLocation();
+  const pathname = location.pathname;
+  const paths = pathname.split('/').filter(path => path);
+
+
   return (
     <div className="w-full ">
       <div className="w-[15%] h-[100vh] bg-[#176b87] float-left relative">
@@ -20,6 +28,12 @@ const Homepage = () => {
         </div>
       </div>
       <div className="w-[85%] h-[100vh] bg-slate-200 float-right">
+         <Breadcrumbs
+            paths={paths.map((path, index) => ({
+              name: path, 
+              url: `/${paths.slice(0, index + 1).join('/')}`,
+            }))}
+          />
         <Outlet />
       </div>
     </div>
