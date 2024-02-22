daa6c5bd523b9dc4ea8c1e8d692b1faf8b3e5d7c
Krishna <89905895+krishna7288@users.noreply.github.com>
2024-02-22 16:18:41 +0530

    Sidebar responsive completed

--- src/common/Homepage.jsx
+++ src/common/Homepage.jsx
@@ -1,33 +1,34 @@
 import Adminsidebar from "./Sidebar";
 import { Outlet, useNavigate } from "react-router-dom";
-import { IoMdLogOut } from "react-icons/io";
 import { useLocation } from 'react-router-dom';
 import Breadcrumbs from "../components/utils/Breadcrumbs";
+import { useState } from "react";
 
 const Homepage = () => {
     let navigate = useNavigate()
-  const Logout = async () => {
-    localStorage.removeItem("chits");
-    localStorage.removeItem("chitsRole");
-    // navigate('/')
-  };
+  
 
   const location = useLocation();
   const pathname = location.pathname;
   const paths = pathname.split('/').filter(path => path);
 
 
+  const [open,setOpen] = useState(true);
+  const handleToggle = () =>{
+    setOpen(!open)
+  }
+
+
   return (
-    <div className="w-full ">
-      <div className="w-[15%] h-[100vh] bg-[#176b87] float-left relative">
-        <Adminsidebar />
-        <div className="absolute bottom-5 w-[90%] flex px-10">
-          <h1 className="flex items-center text-slate-200 cursor-pointer gap-1" onClick={Logout}>
-            <IoMdLogOut /> Logout
-          </h1>
-        </div>
-      </div>
-      <div className="w-[85%] h-[100vh] bg-slate-200 float-right">
+    <div className="w-full flex bg-white">
+     
+        <Adminsidebar open={open} handleToggle={handleToggle}/>
+        
+          
+
+      <div className={`${
+          open ? "absolute  grow left-[262px] right-0 h-[100vh]" : "absolute grow lg:left-[6%] left-[15%] right-0 h-auto"
+        } duration-500  bg-white`}>
          <Breadcrumbs
             paths={paths.map((path, index) => ({
               name: path, 
