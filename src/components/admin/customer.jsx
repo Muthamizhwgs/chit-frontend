1bba0f3af70787141eff997dcdd24548228f9d34
Krishna <89905895+krishna7288@users.noreply.github.com>
2024-03-05 12:24:02 +0530

    searchbar style

--- src/components/admin/customer.jsx
+++ src/components/admin/customer.jsx
@@ -323,11 +323,25 @@ function Customers() {
         </div>
 
         <div className='w-[95%] m-auto xs:block flex items-center justify-center'>
-          <div className='relative w-fit'>
+          {/* <div className='relative w-fit'>
             <input type="text" placeholder='Enter Customer Name or Phone Number' className='sm:w-80 xs:w-72 w-60 h-10 pl-3  border-2 border-black-100 rounded-full pb-1  placeholder:text-[10.5px] xs:placeholder:text-xs  sm:placeholder:text-sm' value={searchTerm}
               onChange={(e) => handleSearch(e.target.value)} />
             <FaSearch className='text-gray-300 absolute -translate-x-1/2 -translate-y-1/2 top-[50%] right-[2%]' />
-          </div>
+          </div> */}
+          <form className="form sm:w-80 xs:w-72 w-64">
+            <label for="search">
+                <input className="input sm:w-80 xs:w-72 w-64 placeholder:text-[10.5px] xs:placeholder:text-xs  sm:placeholder:text-sm pb-1" type="text" required="" placeholder='Enter Customer Name or Phone Number' id="search" value={searchTerm}
+              onChange={(e) => handleSearch(e.target.value)}/>
+                <div class="fancy-bg"></div>
+                <div class="search">
+                    <svg viewBox="0 0 24 24" aria-hidden="true" class="r-14j79pv r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-4wgw6l r-f727ji r-bnwqim r-1plcrui r-lrvibr">
+                        <g>
+                            <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+                        </g>
+                    </svg>
+                </div>
+            </label>
+          </form>
         </div>
 
         <div className='w-[95%] m-auto mt-5 overflow-auto'>
