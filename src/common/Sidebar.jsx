e422cfe4680643bf81cd51563ae507404715b855
pandeeswari1926 <apandeeswari1926@gmail.com>
2024-03-05 13:05:05 +0530

    asd

--- src/common/Sidebar.jsx
+++ src/common/Sidebar.jsx
@@ -185,7 +185,7 @@ const Adminsidebar = React.memo(({ handleToggle, open }) => {
                                     <NavLink
                                         to={menu.path}
                                     >
-                                        <div className={` flex  items-center ${location.pathname.includes(menu.path) ? 'bg-slate-200 rounded-full text-[#176b87]' : 'text-white'} ${open ? "py-2 mx-4" : "py-1 px-4 mx-2 justify-center"}`}
+                                        <div className={`relative hover:rounded-full hover:text-[#176b87]  flex  items-center ${location.pathname.includes(menu.path) ? 'bg-slate-200 rounded-full text-[#176b87]' : 'text-white sidebar '} ${open ? "py-2 mx-4" : "py-1 px-4 mx-2 justify-center"}`}
                                         >
                                             <div className={`${open ? 'ml-4' : ''}`}>
                                                 {
