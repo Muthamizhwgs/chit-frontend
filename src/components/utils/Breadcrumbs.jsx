43f8f41d4a1e9220c4e63274ccfd42d1bc0df53a
Krishna <89905895+krishna7288@users.noreply.github.com>
2024-03-08 17:34:59 +0530

    Breadcrumbs fixed

--- src/components/utils/Breadcrumbs.jsx
+++ src/components/utils/Breadcrumbs.jsx
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Breadcrumbs = ({ paths }) => {
   return (
-    <div className='pl-8 py-5'>
+    <div className='pl-8 py-5 '>
+      <span className='font-medium text-gray-600 mr-1'>Admin &gt;</span>
       {paths.map((path, index) => (
-        <span key={index} className={`${index === paths.length - 1 ? 'text-[#176b87]': 'text-gray-600' } font-medium`}>
-          <Link to={path.url}>{path.name}</Link>
-          {index < paths.length - 1 && ' > '}
+        <span key={index} className={`${index === paths.length - 1 ? 'text-[#176b87] border-b-2 pb-0.5 border-[#176b87]': 'text-gray-600' } ${index=== 0 ? 'hidden' : ''} font-medium mr-1`}>
+          <Link to={path.url} className=''><span className=''></span>{path.name}</Link>
+          {index < paths.length - 1 && ' >'}
         </span>
       ))}
     </div>
